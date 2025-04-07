928ee5fe607de51ea08ae873f820c87b7b63702b
forensor <forensor@hotmail.com>
2025-04-07 13:02:23 +0200

    Changes in package.json

--- bin/mass-upgrade-elm-json.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# You need https://github.com/sharkdp/fd for this script.
-
-for directory in $(fd --glob '**/elm.json' --format '{//}'); do
-    cd "$directory" || exit
-    elm-json upgrade --unsafe --yes
-    cd - || exit
-done
